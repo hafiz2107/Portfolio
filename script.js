88bb172b1e37595d3637442f5f214b463ef21c45
Hafiz Muhammad <hafizshafi001@gmail.com>
2021-08-07 10:29:49 +0530

    Third commit

--- script.js
+++ script.js
@@ -1,31 +1,48 @@
 $(document).ready(function(){
 
-  $("#submit-form").validate({
-    rules:{
-        name:{
-            required:true,
-            minlength:4,
-        },
-        email:{
-            required:true,
-            email:true
-        }
-       
-    },
-    submitHandler:function(form){
-        $.ajax({
-            url:"https://script.google.com/macros/s/AKfycbwNw8kk5Vh6erRklyWsFN0CEo6Y2UCeBHHk_1d9gwF3B4MkNHImCTlSAd2gMaGMzel3/exec",
-            data:$("#submit-form").serialize(),
-            method:"post",
-            success:function (response){
-                alert("Thank You !!")
-                window.location("#aboutme")
-            },
-            error:function (err){
-                alert("Something Error")
-            }
-        })
-    }
-})
-})
-
+    $("#submit-form").validate({
+      rules:{
+          name:{
+              required:true,
+              minlength:4,
+          },
+          email:{
+              required:true,
+              email:true
+          },
+          company:{
+              required:true,
+              minlength:5
+          },
+          mobile:{
+              required:true,
+              number:true,
+              minlength:10,
+              maxlength:10
+              
+          },
+          cmnt:{
+              required:true,
+              minlenght:10
+          }
+         
+      },
+   submitHandler:function(form){
+              $.ajax({
+                  url:"https://script.google.com/macros/s/AKfycbwNw8kk5Vh6erRklyWsFN0CEo6Y2UCeBHHk_1d9gwF3B4MkNHImCTlSAd2gMaGMzel3/exec",
+                  data:$("#submit-form").serialize(),
+                  method:"post",
+                  success:function (response){
+                      alert("Submitted successfully")
+                      window.location.reload()
+                  },
+                  error:function (err){
+                      alert("Something Error")
+                  }
+              })
+          }
+      })
+  })
+  
+  
+  
